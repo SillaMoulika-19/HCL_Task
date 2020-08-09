c11bf38b0bde82e9d0ff94a4c0c84f3587873fa9
SillaMoulika-19 <69158580+SillaMoulika-19@users.noreply.github.com>
2020-08-09 21:09:57 +0530

    Add files via upload

---
+++ db.sql
@@ -0,0 +1,4 @@
+create table AccountTransactions(TransactionId int not null primary key auto_increment,TransactionType varchar(50) not null,TransactionAmount int not null);
+create table TotalAmount(Balance int not null);
+
+
